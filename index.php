30d246e82a8da28e674182d0661054895ef7e4c8
OTFSheikh <155764817+OTFSheikh@users.noreply.github.com>
2024-05-21 10:10:44 +0000

    Create task

--- index.php
+++ index.php
@@ -16,6 +16,39 @@
             <br>
             <tr><td><input type="submit" value="Ajouter" name="ajouter"></td><td><input type="reset" value="Effacer"></td></tr>
         </table>
+        
     </form>
+
+    <?php
+    if (isset($_POST['ajouter'])) {
+        if (empty($_POST["nom"])) {
+            $message = "le nom de la tâches ne doit pas être vide ! ";
+        } elseif (empty($_POST["description"])) {
+            $message = "Entrer une courte description pour vous motivez :)";
+        } elseif (empty($_POST["date"])) {
+            $message = "la date d'échéance est requise !";
+        } else {
+            require_once "include/start_bdd.php";
+            $nom = strip_tags($_POST["nom"]);
+            $description = strip_tags($_POST["description"]);
+            $date = strip_tags($_POST["date"]);
+
+            $req = $bdd->prepare("INSERT INTO liste_taches(tache, description, date) VALUES (:nom, :description, :date)");
+
+            $req->bindvalue(":nom", $nom);
+            $req->bindvalue(":description", $description);
+            $req->bindvalue(":date", $date);
+
+            $resultat = $req->execute();
+            if (!$resultat) {
+                $message = "Erreur de connexion à la base de donnée";
+            }else {
+                $message = "La tâches a été ajoutée";
+            }
+
+        }
+        echo "<h5>".$message."</h5>";
+    }
+    ?>
 </body>
 </html>
